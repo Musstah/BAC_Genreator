14b405336dd38b208fea91f15737c4061d220554
Musstah <121905795+Musstah@users.noreply.github.com>
2023-04-28 10:51:58 +0200

    Delete STL_Generator directory

--- STL_Generator/data_manager.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-class Import_data:
-
-    def from_excel(self):
-        df = pd.read_excel('pt.xlsx')
-        my_column = df.iloc[:, 0].tolist()
-        return my_column
--- STL_Generator/main.py
+++
@@ -1,6 +0,0 @@
-from data_manager import Import_data
-
-
-import_data = Import_data()
-
-print(import_data.from_excel())
